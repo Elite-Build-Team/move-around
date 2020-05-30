65647fab52ef3a9976b30722dd2dcb8cce8ca783
LefterisMantas <44322307+LefterisMantas@users.noreply.github.com>
2020-05-30 15:19:13 +0300

    message

--- src/model/PendingIssues.py
+++ src/model/PendingIssues.py
@@ -1,8 +1,6 @@
 # -*- coding: UTF-8 -*-
 from typing import List
-
 from model import ReportIssue
-
 
 class PendingIssues(object):
     def get_pending_issues(self) -> List[ReportIssue]:
@@ -11,15 +9,15 @@ class PendingIssues(object):
     def set_pending_issues(self, pending_issues: List[ReportIssue]):
         self.__pending_issues = pending_issues
 
-    def add_pending_issue(self, pending_issue: ReportIssue):
+    def add_pending_issue(self, pending_issue: ReportIssue, ReportIssue.__id):
         self.__pending_issues.append(pending_issue)
+        self.__in_pending = True
 
     def delete_pending_issue(self, pending_issue: ReportIssue):
         self.__pending_issues.remove(pending_issue)
 
     def get_issue_details(self):
-        # TODO
-        return self.__issue_details
+        ReportIssue.get_issue_details()
 
     def __init__(self):
         self.__pending_issues: List[ReportIssue] = []
