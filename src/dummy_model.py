87825ea7016b4ea03eaf45544d1e7d533fadf1ed
st1064870 <st1064870@ceid.upatras.com>
2020-05-31 02:26:07 +0300

    Add random generating coordinates

--- src/dummy_model.py
+++ src/dummy_model.py
@@ -1,18 +1,22 @@
 from model import *
-
+from random import random
 class DummyModel:
     def __init__(self):
         self.amea: AMEA = AMEA()
         self.issues_under_construction = IssuesUnderConstruction()
         self.pending_issues = PendingIssues()
         self.access_map: AccessMap = AccessMap()
-        
-        self.access_map.add_location(Location((38.245982, 21.735388), LocationType.Obstacle))
-        self.access_map.add_location(Location((38.245982, 21.735100), LocationType.Toilet))
-        self.access_map.add_location(Location((38.245982, 21.735000), LocationType.Parking))
-        self.access_map.add_location(Location((38.245982, 21.731388), LocationType.Obstacle))
-        self.access_map.add_location(Location((38.245982, 21.732100), LocationType.Toilet))
-        self.access_map.add_location(Location((38.245982, 21.733000), LocationType.Parking))
-        self.access_map.add_location(Location((38.245982, 21.735388), LocationType.Obstacle))
-        self.access_map.add_location(Location((38.245982, 21.738100), LocationType.Toilet))
-        self.access_map.add_location(Location((38.245982, 21.739000), LocationType.Parking))
+        self.report_issue: ReportIssue = None
+
+        self.access_map.add_location(generate_location(LocationType.Obstacle))
+        self.access_map.add_location(generate_location(LocationType.Obstacle))
+        self.access_map.add_location(generate_location(LocationType.Obstacle))
+        self.access_map.add_location(generate_location(LocationType.Parking))
+        self.access_map.add_location(generate_location(LocationType.Parking))
+        self.access_map.add_location(generate_location(LocationType.Parking))
+        self.access_map.add_location(generate_location(LocationType.Toilet))
+        self.access_map.add_location(generate_location(LocationType.Toilet))
+        self.access_map.add_location(generate_location(LocationType.Toilet))
+
+def generate_location(location_type: LocationType):
+    return Location((38.245982 + random()/100, 21.739000 + random()/100), location_type)
